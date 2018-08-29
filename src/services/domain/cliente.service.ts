49233f2ba6238c79d4c5aa8eb7babba1ca8c76c6
Tiago Mesquita <tiagomac@gmail.com>
2018-08-29 18:11:33 -0300

    Salvando novo cliente

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -6,23 +6,32 @@ import { StorageService } from "../storage.service";
 import { API_CONFIG } from "../../config/api.config";
 
 @Injectable()
-export class ClienteService{
+export class ClienteService {
 
-    constructor(public http: HttpClient, public storage: StorageService){
-        
+    constructor(public http: HttpClient, public storage: StorageService) {
+
     }
 
-    findByEmail(email: String) : Observable<ClienteDTO> {
-
-        
+    findByEmail(email: String): Observable<ClienteDTO> {
 
         return this.http.get<ClienteDTO>(
             `${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
 
     }
 
-    getImageFromBucket(id: string) : Observable<any> {
+    getImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
-        return this.http.get(url, {responseType : 'blob'});
+        return this.http.get(url, { responseType: 'blob' });
+    }
+
+    insert(obj: ClienteDTO) {
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes`,
+            obj,
+            {
+                observe: 'response',
+                responseType: 'text'
+            }
+        )
     }
 }
