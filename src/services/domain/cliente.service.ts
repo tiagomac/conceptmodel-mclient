67c3da359a4fa99a3394b3637f4885c8d3f29238
Tiago Mesquita <tiagomac@gmail.com>
2018-09-09 18:07:27 -0300

    Fazendo upload da foto

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from "@angular/core";
-import { ClienteDTO } from "../../models/cliente.dto";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
-import { StorageService } from "../storage.service";
+import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
+import { StorageService } from "../storage.service";
+import { ImageUtilService } from "../image-util.service";
 
 @Injectable()
 export class ClienteService {
 
-    constructor(public http: HttpClient, public storage: StorageService) {
-
+    constructor(
+        public http: HttpClient,
+        public storage: StorageService,
+        public imageUtilService: ImageUtilService) {
     }
 
-    findById(id: String) {
-        return this.http.get(
-            `${API_CONFIG.baseUrl}/clientes/${id}`);
+    findById(id: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
     }
 
-    
-    findByEmail(email: String) {
-        return this.http.get(
-            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+    findByEmail(email: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
     getImageFromBucket(id: string): Observable<any> {
@@ -36,6 +36,20 @@ export class ClienteService {
                 observe: 'response',
                 responseType: 'text'
             }
-        )
+        );
+    }
+
+    uploadPicture(picture) {
+        let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+        let formData: FormData = new FormData();
+        formData.set('file', pictureBlob, 'file.png');
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes/picture`,
+            formData,
+            {
+                observe: 'response',
+                responseType: 'text'
+            }
+        );
     }
 }
