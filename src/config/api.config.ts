bf3b18af64607a5191daaddffdbb0a03ad37e981
Tiago Mesquita <tiagomac@gmail.com>
2018-09-13 19:33:19 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://curso-spring-ionic-tiagomac.herokuapp.com/",
+    //baseUrl: "http://localhost:8080",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/conceptmodel"
 }
