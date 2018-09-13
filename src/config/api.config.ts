2070a54fa6936ee1518d711e4277fe6c2a69efd2
Tiago Mesquita <tiagomac@gmail.com>
2018-09-13 19:41:13 -0300

    Correcao: Remocao da ultima barra do endereco

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "https://curso-spring-ionic-tiagomac.herokuapp.com/",
+    baseUrl: "https://curso-spring-ionic-tiagomac.herokuapp.com",
     //baseUrl: "http://localhost:8080",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/conceptmodel"
 }
